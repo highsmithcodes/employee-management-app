39e9dec14a23c5fbf9668adf1756c1c249a63014
highsmithcodes <ezrahighsmith@gmail.com>
2023-01-21 18:13:20 -0700

    update README

--- src/Pages/LandingPageNoLogin.js
+++ src/Pages/LandingPageNoLogin.js
@@ -4,8 +4,18 @@ export default function LandingPageNoLogin() {
 
     return (
         <>
-            <div style={{ padding: 50 }} className="main">
-                Advertise the product on this page
+            <div className="hero">
+                <h1>Kept</h1>
+                <div className="small-title">Keep your team organized with a knowledge base</div>
+            </div>
+
+            <div className="pricing-table">
+                <h2>Pricing</h2>
+                <div className="pricing-options">
+                    <div className="price">
+                        
+                    </div>
+                </div>
             </div>
         </>
     )
