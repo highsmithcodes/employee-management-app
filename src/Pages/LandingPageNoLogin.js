fdd753f4a3ab34c4c27f4e615a83af2e63852db7
Highsmith Codes <ezrahighsmith@gmail.com>
2023-06-01 17:22:28 +0000

    homepage styling

--- src/Pages/LandingPageNoLogin.js
+++ src/Pages/LandingPageNoLogin.js
@@ -13,13 +13,15 @@ export default function LandingPageNoLogin() {
             
 
     <section class="bg-white">
-        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-            <div class="mr-auto place-self-center lg:col-span-7">
-                <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl">Create, store and manage records</h1>
-                <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">Kept, the knowledge base tool to keep your teams organized.</p>
+        <div class="relative flex justify-center item-center max-w-5xl mx-auto py-20 sm:py-24 lg:py-32 h-screen">
+            <div class="mx-auto place-self-center lg:col-span-7">
+                <h1 class="mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl text-center">Create, store and manage records</h1>
+                <p class="max-w-2xl font-light text-gray-500 text-center mx-auto">Kept, the knowledge base tool to keep your teams organized.</p>
+                <div class="flex justify-center items-center mx-auto py-5">
                 <Link to="/login/" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-rose-600 hover:bg-rose-800 focus:ring-4 focus:ring-rose-300">Get started
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </Link>            
+                </Link>           
+                </div> 
             </div>
             <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
                 {/* <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup" /> */}
